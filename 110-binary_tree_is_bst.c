98786c4582ef90bae61ddfb272127032029970e4
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-03 12:43:11 +0100

    check if tree is a binary search tree

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a
+ * binary search tree
+ * @tree: pointer to root node of tree
+ * Return: 1 if tree is a BST, otherwise 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	binary_tree_t *prev = NULL;
+
+	if (!tree)
+		return (0);
+
+	return (is_bst((binary_tree_t *) tree, prev));
+}
+/**
+ * is_bst - auxilliary function for checking if a tree is a BST
+ * @node: pointer to node
+ * @prev: pointer to previous node on stack
+ * Return: 1 if node is a BST, otherwise 0
+ */
+int is_bst(binary_tree_t *node, binary_tree_t *prev)
+{
+	int left, right;
+
+	if (!node)
+		return (1);
+
+	left = is_bst(node->left, prev);
+	if (prev && node->n < prev->n)
+		return (0);
+	prev = node;
+
+	right = is_bst(node->right, prev);
+
+	if (left && right)
+		return (1);
+	else
+		return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -34,7 +34,7 @@ binary_tree_t *ancestor(const binary_tree_t *node, const binary_tree_t *first,
 		const binary_tree_t *second);
 void level_print(const binary_tree_t *tree, size_t level, void (*func)(int));
 int is_complete(const binary_tree_t *node, size_t node_idx, size_t tree_size);
-
+int is_bst(binary_tree_t *node, binary_tree_t *prev);
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -62,4 +62,5 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
