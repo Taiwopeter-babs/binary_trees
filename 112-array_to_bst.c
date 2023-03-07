e6c9edc21aea271e67785e6717b30ee1e3c9c847
Taiwo Babalola <babalolataiwop@gmail.com>
2023-03-07 21:07:34 +0100

    build BST from array

---
+++ 112-array_to_bst.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a binary search tre from an array
+ * @array: pointer to first element in array
+ * @size: size of array
+ * Return: pointer to root node of tree, otherwise NULL
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i;
+	bst_t *root, *root_node, *node;
+
+	if (!array)
+		return (NULL);
+
+	root = NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i])
+		{
+			if (i == 0)
+			{
+				root_node = bst_insert(&root, array[i]);
+				if (root_node == NULL)
+					return (NULL);
+			}
+			else
+			{
+				node = bst_insert(&root, array[i]);
+				if (!node)
+					return (NULL);
+			}
+		}
+	}
+	return (root_node);
+}
--- move.sh
+++ move.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # This script moves all files without an extension to targetted directory
+
 find . -maxdepth 1 -type f ! -name "*.*" -exec mv -t "$1" {} \+
+mv *-main.c "$1"
